bc5186b39e0d03adb64295c2b10f8d58d3195679
Srecko Markovic <sreckomarkovic@ufl.edu>
2024-04-01 15:16:42 -0400

    sites navbar is working well

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 #used to store roots in folder
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
