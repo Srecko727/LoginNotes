40d83aed78e50b71f9d50de02087444ef539254c
Srecko Markovic <sreckomarkovic@ufl.edu>
2024-04-04 15:27:11 -0400

    alerts working for sign up page

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean = True)
 
@@ -10,6 +10,23 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method=='POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:#len is length
+            flash('Error! Email must be greater than three characters.', category='error')
+        elif len(firstName) < 2:
+            flash('Error! First name must be greater than one character.', category='error')
+        elif len(password1) < 7:
+            flash('Error! Password must be greater than seven characters.', category='error')
+        elif password1 != password2:
+            flash('Error! Passwords do not match.', category='error')
+        else:
+            flash('Account created!', category='success')
+            # add user to database
     return render_template("sign_up.html")
