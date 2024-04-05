9565ff2c616ac671c981a923f99fabf66afd1e56
Srecko Markovic <sreckomarkovic@ufl.edu>
2024-04-05 13:41:06 -0400

    can add a note/ delete a note

--- website/static/index.js
+++ website/static/index.js
@@ -1,9 +1,8 @@
-function deleteNote(noteId)
-{
+function deleteNote(noteId) {
     fetch("/delete-note", {
-        method: "POST",
-        body: JSON.stringify({ noteId: noteId }),
-      }).then((_res) => {
-        window.location.href = "/";
-      });
-}
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
